24e64e19f1890aafc74058cde19dd914a8ba0fed
Alexandre Geiser <ageiser@car12s4.42barcelona.com>
2024-12-03 13:31:15 +0100

    command files created

---
+++ src/Command/MODE.cpp
@@ -0,0 +1,8 @@
+#include "../../includes/Command/Command.hpp"
+
+/*
+i = channel on invitation only
+t = only operators can change the channel topic
+k = channel protected by a password
+o = operators management
+l = numbers of users limitation */
