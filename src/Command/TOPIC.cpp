afd4cb34100489496b8cdb0b48e28c58e85f9715
Paula Roche castro <proche-c@car5s4.42barcelona.com>
2025-01-11 19:11:01 +0100

    1

--- src/Command/TOPIC.cpp
+++ src/Command/TOPIC.cpp
@@ -31,7 +31,7 @@ void handleTopic(Client *client, Server * server)
     //std::cout << channelName <<std::endl;
 
     // search the channel via the server
-    Channel *channel = server->findChannel(channelName);
+    Channel *channel = server->findChannel(channelName); 
     if (channel == NULL)
     {
         std::string errorMsg = ERR_NOSUCHCHANNEL(client->getNickName(), channelName);
@@ -39,7 +39,20 @@ void handleTopic(Client *client, Server * server)
         std::cout << "Error: No such channel" << std::endl;
         return;
     }
-
+    // if (args.size() == 1)
+    // {
+    //     channel->sendTopic(*client);
+    //     //std::cout << "Topic sent to client" << std::endl;
+    //     return;
+    // }
+    std::string newTopic = client->getLastArg();
+    if (channel->getProtectedTopicMode() && !channel->isOperator(client))
+    {
+        std::string errorMsg = ERR_CHANOPRIVSNEEDED(client->getNickName(), channelName);
+        send(client->getSocket(), errorMsg.c_str(), errorMsg.size(), 0);
+        std::cout << "Error: Client is not an operator of the channel\n";
+        return;
+    }
     // Check if the client is a member of the channel
     if (!channel->isMember(client)) 
     {
@@ -58,10 +71,10 @@ void handleTopic(Client *client, Server * server)
     }
 
     // Check if the topic starts with ':'
-    std::string newTopic = args[1];
+    // std::string newTopic = args[1];
 
     // Ensure that there is no space between ':' and the topic content
-    if (newTopic.empty() || newTopic[0] != ':') 
+    if (newTopic.empty()) 
     {
         std::string errorMsg = ERR_TOPICNEEDSCOLON(client->getNickName());
         send(client->getSocket(), errorMsg.c_str(), errorMsg.size(), 0);
@@ -70,7 +83,7 @@ void handleTopic(Client *client, Server * server)
     }
 
     // Remove the ':' from the start of the topic (it's part of the IRC syntax)
-    newTopic = newTopic.substr(1); // Remove the ':' character
+    // newTopic = newTopic.substr(1); // Remove the ':' character
 
     // Optionally, trim any leading or trailing spaces from the topic string
     newTopic.erase(0, newTopic.find_first_not_of(" \t")); // Trim the spaces at the beginning
